6690f741e02385f57cd8966db813438ddf5c1549
Viacheslav Mitskevich <viacheslavmitskevich@ya.ru>
2024-09-29 20:57:43 +0800

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -13,7 +13,7 @@ var maxProfit = function (prices) {
   for (i = 0; i < prices.length; i++) {
     for (j = 1 + i; j < prices.length; j++) {
       if (prices[j] - prices[i] > 0 && prices[j] - prices[i] > max) {
-        max =+ prices[j] - prices[i];
+        max = prices[j] - prices[i];
       } 
     }  
   }
